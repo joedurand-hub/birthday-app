3fe22f3396646fabb849e300b3c54b9b43e91186
joedurand-hub <53331000+joedurand-hub@users.noreply.github.com>
2022-01-12 09:40:51 -0300

    configure prettier

--- pages/allBirthdays.js
+++ pages/allBirthdays.js
@@ -4,13 +4,13 @@ import { AnchorToBack } from "../Components/AnchorsButton/Anchor";
 import styles from "../styles/container.module.css";
 
 function allBirthdays({ data }) {
-	return (
-		<div className={styles.containerApp}>
-			<Link href="/birthdays" passHref>
-				<AnchorToBack />
-			</Link>
-		</div>
-	);
+  return (
+    <div className={styles.containerApp}>
+      <Link href="/birthdays" passHref>
+        <AnchorToBack />
+      </Link>
+    </div>
+  );
 }
 
 export default allBirthdays;
